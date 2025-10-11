366d2fb298579ef459da151afaf2960ad7fb7bda
Puneet Verma <verma0404puneet@gmail.com>
2025-10-12 01:07:44 +0530

    Automapper configured to utilize the entity profiles

--- SteamClone.Backend/Program.cs
+++ SteamClone.Backend/Program.cs
@@ -4,9 +4,23 @@ using SteamClone.Backend.Settings;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using AutoMapper;
+using SteamClone.Backend.Profiles;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Manual AutoMapper configuration
+var mapperConfig = new MapperConfiguration(cfg =>
+{
+    cfg.AddProfile<UserProfile>();
+    cfg.AddProfile<CouponsProfile>();
+    cfg.AddProfile<GameProfile>();
+    cfg.AddProfile<CartProfile>();
+    cfg.AddProfile<OrderProfile>();
+    cfg.AddProfile<ReviewProfile>();
+});
+var mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
 
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
 builder.Services.AddSingleton<IUserService, UserService>();
@@ -15,7 +29,6 @@ builder.Services.AddSingleton<ICartService, CartService>();
 builder.Services.AddSingleton<IOrderService, OrderService>();
 builder.Services.AddSingleton<IReviewService, ReviewService>();
 builder.Services.AddSingleton<JwtService>();
-
 
 var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
 builder.Services.AddAuthentication(options =>
@@ -57,7 +70,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 
 app.UseAuthentication();
 app.UseAuthorization();
