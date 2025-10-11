4dd442a3eabdb1dd5332b6dbca7a7a0034da37e1
Puneet Verma <verma0404puneet@gmail.com>
2025-10-12 01:04:01 +0530

    Created coupon DTOs

---
+++ SteamClone.Backend/DTOs/CouponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SteamClone.Backend.DTOs;
+
+public class CouponDto
+{
+    public required string Code { get; set; }
+    public required string CouponName { get; set; }
+    public required int DiscountPercent { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public bool IsActive { get; set; }
+}
