d21ed87820d5ac754797a2c61c50aec33f393465
Puneet Verma <verma0404puneet@gmail.com>
2025-10-12 01:47:40 +0530

    Fixed property Name to CouponName

--- SteamClone.Backend/Entities/Coupons.cs
+++ SteamClone.Backend/Entities/Coupons.cs
@@ -4,7 +4,7 @@ public class Coupons
 {
     public int CouponId { get; set; }
     public required string Code { get; set; }
-    public required string Name { get; set; }
+    public required string CouponName { get; set; }
     public required int DiscountPercent { get; set; }
     public required bool IsActive { get; set; } = true;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
